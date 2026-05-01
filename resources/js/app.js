50b25266461e8e5430969695e9fdd6ad0d4fd52e
DereckZR <dereckzeballos@gmail.com>
2026-05-01 02:21:12 -0300

    Add DataTables integration with Bootstrap 4 support

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,7 @@ window.$ = window.jQuery = $;
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'admin-lte/dist/js/adminlte.min.js';
+
+// DataTables
+import 'datatables.net-bs4';
+import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css';
