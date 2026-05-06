b0c839d2034570071b850c728879e8563de3a234
DereckZR <dereckzeballos@gmail.com>
2026-05-06 10:46:02 -0300

    Add TableButton component and corresponding Blade view for dynamic button rendering

---
+++ app/View/Components/TableButton.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Enums\TableButtonAction;
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class TableButton extends Component
+{
+    public string $label;
+    public TableButtonAction $action;
+    public int $id;
+    public string $type;
+
+    public function __construct(
+        string $label,
+        TableButtonAction $action,
+        int $id,
+        string $type = 'primary',
+    ) {
+        $this->label = $label;
+        $this->action = $action;
+        $this->id = $id;
+        $this->type = $type;
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.table-button');
+    }
+}
