a18ca956028d61d45fa965335ddbe909937d5497
DereckZR <dereckzeballos@gmail.com>
2026-05-22 14:47:33 -0300

    Refactor getCycleLabel function to utilize imported ordinalNames; remove redundant cycle number calculation and add ordinalNumbersNames module for better organization.

--- resources/js/utils/getCycleLabel.js
+++ resources/js/utils/getCycleLabel.js
@@ -1,5 +1,7 @@
 // utils/getCycleLabel.js
 
+import { ordinalNumbersNames as ordinalNames } from './ordinalNumbers.js';
+
 /*
 |--------------------------------------------------------------------------
 | getCycleLabel
@@ -12,38 +14,9 @@ export function getCycleLabel(data, meta) {
         return '<span class="text-muted">Ciclo eliminado</span>';
     }
 
-    const visibleCycles = meta.settings.aoData
-        .map(item => item._aData)
-        .filter(item => !item.deleted_at);
-
-    const cycleNumber = visibleCycles.findIndex(
-        item => item.id === data.id
-    ) + 1;
+    const cycleNumber = data.cycle_number;
 
     return ordinalNames[cycleNumber]
         ? `${ordinalNames[cycleNumber]} ciclo`
         : `${cycleNumber}° Ciclo`;
 }
-
-const ordinalNames = {
-    1: 'Primer',
-    2: 'Segundo',
-    3: 'Tercer',
-    4: 'Cuarto',
-    5: 'Quinto',
-    6: 'Sexto',
-    7: 'Séptimo',
-    8: 'Octavo',
-    9: 'Noveno',
-    10: 'Décimo',
-    11: 'Undécimo',
-    12: 'Duodécimo',
-    13: 'Decimotercer',
-    14: 'Decimocuarto',
-    15: 'Decimoquinto',
-    16: 'Decimosexto',
-    17: 'Decimoséptimo',
-    18: 'Decimoctavo',
-    19: 'Decimonoveno',
-    20: 'Vigésimo'
-};
---
+++ resources/js/utils/ordinalNumbers.js
@@ -0,0 +1,30 @@
+// utils/ordinalNumbers.js
+
+/*
+|--------------------------------------------------------------------------
+| ordinalNumbersNames
+| Objeto que contiene los nombres ordinales en español
+|--------------------------------------------------------------------------
+*/
+export const ordinalNumbersNames = {
+    1: 'Primer',
+    2: 'Segundo',
+    3: 'Tercer',
+    4: 'Cuarto',
+    5: 'Quinto',
+    6: 'Sexto',
+    7: 'Séptimo',
+    8: 'Octavo',
+    9: 'Noveno',
+    10: 'Décimo',
+    11: 'Undécimo',
+    12: 'Duodécimo',
+    13: 'Decimotercer',
+    14: 'Decimocuarto',
+    15: 'Decimoquinto',
+    16: 'Decimosexto',
+    17: 'Decimoséptimo',
+    18: 'Decimoctavo',
+    19: 'Decimonoveno',
+    20: 'Vigésimo'
+};
