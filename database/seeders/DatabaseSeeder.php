01b09fe877d805f63bc69bc145ca24400749ac02
LeoGMR20Dev <leoguimorales@gmail.com>
2026-07-01 23:57:54 -0400

    Add RoleSeeder to seed roles in the database and update DatabaseSeeder to call RoleSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,9 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void {}
+    public function run(): void {
+        $this->call([
+            RoleSeeder::class
+        ]);
+    }
 }
