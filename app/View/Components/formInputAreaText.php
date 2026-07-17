dd2ac7c583ccc5bb9b315228e1973e15488991b9
DereckZR <dereckzeballos@gmail.com>
2026-07-17 10:08:44 -0400

    Fix class name casing for FormInputAreaText component

--- app/View/Components/formInputAreaText.php
+++ app/View/Components/formInputAreaText.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\View\Component;
 use Illuminate\Contracts\View\View;
 
-class formInputAreaText extends Component
+class FormInputAreaText extends Component
 {
     public string $name;
     public string $label;
