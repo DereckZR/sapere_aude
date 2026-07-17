1489097b393bb2f3e0e7b8b663c7197b106fe8de
DereckZR <dereckzeballos@gmail.com>
2026-07-17 12:48:36 -0400

    Remove autocomplete attribute to input fields in form components

--- resources/views/components/form-input-number.blade.php
+++ resources/views/components/form-input-number.blade.php
@@ -2,6 +2,6 @@
     <label for="{{ $name }}">{{ $label }}</label>
     <input type="text" class="form-control input-number" name="{{ $name }}" id="{{ $name }}"
         placeholder="{{ $placeholder }}" data-is-decimal="{{ $isDecimal ? '1' : '0' }}" value="{{ $value }}"
-        required>
+        autocomplete="off" required>
     <span class="invalid-feedback"></span>
 </div>
