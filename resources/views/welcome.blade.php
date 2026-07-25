98d1a5b1af20ce48f3c0ef162f90425dd8958117
LeoGMR20Dev <leoguimorales@gmail.com>
2026-07-24 22:26:14 -0400

    Add transaction button to dashboard view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,13 @@
 @stop
 
 @section('content')
-    <p>Bienvenido 👋</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <a href="{{ route('transactions.index') }}" class="btn btn-primary mb-3">
+                    <i class="fas fa-plus"></i> Añadir Transacción
+                </a>
+            </div>
+        </div>
+    </div>
 @stop
