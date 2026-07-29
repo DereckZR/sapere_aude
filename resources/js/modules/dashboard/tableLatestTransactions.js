b8cb73a7fe3de4ec858a502b3e2e18e0953250b1
DereckZR <dereckzeballos@gmail.com>
2026-07-28 23:25:37 -0400

    Uncomment DataTable initialization and related fields in latest transactions table; comment out input fields in transaction modal

--- resources/js/modules/dashboard/tableLatestTransactions.js
+++ resources/js/modules/dashboard/tableLatestTransactions.js
@@ -4,64 +4,64 @@ import { renderTableActions } from "../../utils/renderTableActions.js";
 import { MovementType } from "../../enums/movementType.js";
 
 export function initLatestTransactionsTable() {
-    // return $("#tableTransactions").DataTable({
-    //     processing: true,
-    //     data: [],
-    //     order: [],
-    //     autoWidth: false,
-    //     searching: false,
-    //     paging: false,
-    //     info: false,
-    //     lengthChange: false,
-    //     columns: [
-    //         {
-    //             data: "transaction_category.name",
-    //             title: "Categoría",
-    //         },
-    //         {
-    //             data: "transaction_category.type",
-    //             title: "Tipo",
-    //             render: function (data) {
-    //                 if (data === MovementType.IN) {
-    //                     return "Ingreso";
-    //                 } else if (data === MovementType.OUT) {
-    //                     return "Egreso";
-    //                 } else {
-    //                     return data;
-    //                 }
-    //             },
-    //         },
-    //         {
-    //             data: "responsible_member",
-    //             title: "Responsable",
-    //             render: function (data) {
-    //                 return `${data.first_name} ${data.last_name}`;
-    //             },
-    //         },
-    //         {
-    //             data: "amount",
-    //             title: "Monto (Bs)",
-    //         },
-    //         {
-    //             data: "transaction_date",
-    //             title: "Fecha de la transacción",
-    //             render: function (data) {
-    //                 return formatDate(data);
-    //             },
-    //         },
-    //         {
-    //             data: "created_at",
-    //             title: "Fecha y hora de registro",
-    //             render: function (data) {
-    //                 return formatDatetime(data);
-    //             },
-    //         },
-    //     ],
-    //     createdRow: function (row, data) {
-    //         if (data.deleted_at) {
-    //             $(row).addClass("table-deleted");
-    //         }
-    //     },
-    //     language: DATATABLE_ES,
-    // });
+    return $("#tableTransactions").DataTable({
+        processing: true,
+        data: [],
+        order: [],
+        autoWidth: false,
+        searching: false,
+        paging: false,
+        info: false,
+        lengthChange: false,
+        columns: [
+            {
+                data: "transaction_category.name",
+                title: "Categoría",
+            },
+            {
+                data: "transaction_category.type",
+                title: "Tipo",
+                render: function (data) {
+                    if (data === MovementType.IN) {
+                        return "Ingreso";
+                    } else if (data === MovementType.OUT) {
+                        return "Egreso";
+                    } else {
+                        return data;
+                    }
+                },
+            },
+            {
+                data: "responsible_member",
+                title: "Responsable",
+                render: function (data) {
+                    return `${data.first_name} ${data.last_name}`;
+                },
+            },
+            {
+                data: "amount",
+                title: "Monto (Bs)",
+            },
+            {
+                data: "transaction_date",
+                title: "Fecha de la transacción",
+                render: function (data) {
+                    return formatDate(data);
+                },
+            },
+            {
+                data: "created_at",
+                title: "Fecha y hora de registro",
+                render: function (data) {
+                    return formatDatetime(data);
+                },
+            },
+        ],
+        createdRow: function (row, data) {
+            if (data.deleted_at) {
+                $(row).addClass("table-deleted");
+            }
+        },
+        language: DATATABLE_ES,
+    });
 }
