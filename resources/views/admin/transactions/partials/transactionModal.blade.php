7bc272f4bebc10ad00e8b8aa954b8801f2588545
DereckZR <dereckzeballos@gmail.com>
2026-07-28 23:11:04 -0400

    Comment out transaction category input and related fields in transaction modal

--- resources/views/admin/transactions/partials/transactionModal.blade.php
+++ resources/views/admin/transactions/partials/transactionModal.blade.php
@@ -4,7 +4,7 @@
 @endphp
 
 <x-form-modal :createUrl="$createUrl" :updateUrl="$updateUrl" title="Transacción">
-    <x-form-input-select name="transaction_category_id" label="Categoría" />
+    {{-- <x-form-input-select name="transaction_category_id" label="Categoría" />
     <x-form-input-number name="amount" label="Monto" value="1" :isDecimal="true"
         placeholder="Ingrese el monto de la transacción" />
     <x-form-input-select name="is_cash" label="Es efectivo" />
@@ -12,5 +12,5 @@
     <x-form-input-area-text name="description" label="Descripción (opcional)" :required="false"
         placeholder="Ingrese la descripción" />
     <x-form-input-select name="responsible_member_id" label="Miembro responsable" />
-    <x-form-input-select name="cycle_id" label="Ciclo" />
+    <x-form-input-select name="cycle_id" label="Ciclo" /> --}}
 </x-form-modal>
